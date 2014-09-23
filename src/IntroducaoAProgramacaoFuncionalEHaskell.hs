5ab406450bca1acce5890c751f0c87c15e7d5589
pauloborba <phmb@cin.ufpe.br>
2014-09-23 13:57:26 +0000

    Funções de conjuntos e outros pequenos ajustes

--- src/IntroducaoAProgramacaoFuncionalEHaskell.hs
+++ src/IntroducaoAProgramacaoFuncionalEHaskell.hs
@@ -3,13 +3,17 @@ module IntroducaoAProgramacaoFuncionalEHaskell where
 import Data.Char
 
 -- fatorial, sintaxe da matemática, sem muita redundância
+-- if statement in haskell
 -- then obrigatório
 -- parênteses opcional na condição
--- comentários com -- respondendo perguntas de sintaxe com o google
--- if statement in haskell
--- grammar: http://www.informatik.uni-freiburg.de/~thiemann/haskell/haskell98-report-html/syntax-iso.html
 
 fat n = if n == 0 then 1 else n * fat (n-1)
+
+
+-- comentários com -- ou {- -}
+-- respondendo perguntas de sintaxe com o google
+-- or grammar: http://www.haskell.org/onlinereport/haskell2010/
+-- f n-1 é equivalente a (f n) - 1, se f só recebe um argumento
 
 fatinfinito n = if n == 0 then 1 else n * fatinfinito n-1
 
@@ -25,8 +29,7 @@ fatorial n = n * fatorial (n-1)
 -- Int operators: +, *, -, ^, div, mod :: Int -> Int -> Int
 --                >, >=, ==, /=, <=, < :: Int -> Int -> Bool
 -- Actually works for Num, to be discussed later, with type classes
--- Referência
---
+-- Olhar http://www.haskell.org/onlinereport/haskell2010/haskellch6.html#x13-1160006
 
 max :: Int -> Int -> Int
 max a b = if (a > b) then a else b
@@ -108,12 +111,14 @@ debitarConta (n,s) v = (n,s - v)
 soma [] = 0
 soma (nota : l) = nota + soma l
 
+-- avaliar passo a passo a soma, como máquina de reescrita
+
+
 nossohead (x:xs) = x
 
+-- sem outra equação, a avaliação de nossohead [] leva a 
 -- *** Exception: AplicacaoParcial.hs:114:1-20: Non-exhaustive patterns in function nossohead
 -- por conta de coisas como essa, noção de fortemente tipada não é 100% correta para Haskell. Similar a Java, mas um pouco melhor.
-
--- avaliar passo a passo a soma, como máquina de reescrita
 
 tamanho [] = 0
 tamanho (nota : l) = 1 + tamanho l
@@ -122,7 +127,59 @@ procurarIndice [] i = []
 procurarIndice (e:l) 0 = [e]
 procurarIndice (e:l) i = procurarIndice l (i-1)
 
--- visão geral do interpretador
--- usar google como fonte para tirar dúvidas sobre a sintaxe 
+-- visão geral do interpretador e do ambiente
 
-ex = tamanho [1,2,3]
+ex = tamanho [4,1,2,3]
+
+
+-- equações de funcões diferentes intercaladas dá erro: Multiple declarations of f1
+-- duas equações começando com f 0 ou f n dá warning de overllaping de padrões, não erro
+-- a primeira equação é usada; a ordem importa
+-- o mesmo ocorre quando a equação de f n aparece antes de f 0
+
+
+f1 0 = 1
+f1 n = n * f1 (n-1)
+
+f2 0 = 0
+f2 m = m * f2 (m-1)
+
+
+-- contar elementos em uma lista
+
+count e [] = 0
+count e (x:xs) | e == x = 1 + count e xs
+               | otherwise = count e xs
+
+               
+-- conjuntos: união, interseção, diferença
+
+member e [] = False
+member e (x:xs) | x == e = True
+                | otherwise =  member e xs
+
+                
+-- reuso versus eficiência
+
+outromember e l = (count e l) > 0                
+
+
+union [] c =  c
+union (x:xs) c | member x c = union xs c
+               | otherwise = x:(union xs c)
+
+
+-- simetria               
+               
+intersection [] c = []
+intersection (x:xs) c | member x c = x:(intersection xs c)
+                      | otherwise = intersection xs c
+                      
+
+-- underscore poderia ter sido usado acima também                      
+-- observar as similaridades entre as recursões e diferenças nas bases
+                      
+diff [] _ = []
+diff c [] = c
+diff (x:xs) c | member x c = diff xs c
+              | otherwise = x:(diff xs c)
--- src/Main.hs
+++ src/Main.hs
@@ -8,5 +8,17 @@ import DetalhesSobreListasERecursao
 main :: IO ()
 main = do
     putStrLn "Welcome!"
+    putStrLn (show (nossohead [3,4,1]))
     putStrLn (show (tamanhoBanco))
+    putStrLn (show (f2 0))
+    putStrLn (show (count 3 [1,2,3,1,4,3,6,7,4,3]))
+    putStrLn (show (count 3 [1,2,1,4,6,7,4]))
+    putStrLn (show (member 3 [1,2,1,4,6,7,4]))
+    putStrLn (show (member 3 [1,2,3,1,4,3,6,7,4,3]))
+    putStrLn (show (union [3,4,2] [1,2,3,5]))
+    putStrLn (show (intersection [3,4,2] [1,2,3,5]))
+    putStrLn (show (diff [3,4,2] [1,2,3,5]))
+    putStrLn (show (union [1,2,3] []))
+    putStrLn (show (intersection [3,4,2] [1,22,33,5]))
+    putStrLn (show (diff [3,4,2] [1,2,3,4,5]))
     
