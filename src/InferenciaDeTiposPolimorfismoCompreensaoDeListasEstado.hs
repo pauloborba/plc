0a78cf0cebbdef193cf1aa1194459a43d40e8603
pauloborba <phmb@cin.ufpe.br>
2015-09-03 16:03:01 -0700

    explicacao sobre inferencia de tipos de concat

--- src/InferenciaDeTiposPolimorfismoCompreensaoDeListasEstado.hs
+++ src/InferenciaDeTiposPolimorfismoCompreensaoDeListasEstado.hs
@@ -14,7 +14,20 @@ zipp _ _ = []
 
 -- Comparar com um zipp sem os underlines
 -- Exercitar mecanismo de inferência de tipos com reverse e ++, e depois com o procurar.
-
+-- a ideia é tentar inferir o tipo da funcao. o ambiente haskell começa com a primeira equacao: conc [] l = l. 
+-- por ai, ele ja descobre que essa função tem dois parâmetros. ele inicialmente registra que o tipo deve ser algo 
+-- como conc :: x -> y -> z. indo adiante, olhando para o primeiro parâmetro, ele chega a conclusão que tem que ser 
+-- uma lista. ai ele chega a conclusão parcial de que conc :: [a] -> y -> z. olhando o fato de que o parâmetro e resultado 
+-- são os mesmos, ele chega a conc :: [a] -> y -> y. ai parte para a segunda equacao: conc (e:l) m = e:conc l m. 
+-- como ele ja assumiu que o primeiro parâmetro é do tipo [a], a variável "e" tem que ser do tipo a, já que é
+-- a cabeça da lista passada como primeiro parâmetro. se o primeiro parâmetro da segunda equação fosse 12, ele ja diria 
+-- que a função não é bem tipada (uma equacao pedindo lista, a outra int, não teria como resolver). 
+-- olhando para a direita do =, e vendo :, ele descobre que o resultado tem que ser lista. vendo mais ainda que a cabeça da 
+-- lista resultante é "e", ele infere que o resultado é do tipo [a], já que antes a gente tinha concluído que "e" era do tipo a.
+-- como o resultado é do tipo [a], e o segundo parâmetro é do mesmo tipo do resultado (ver raciocínio mais acima), ele infere
+-- conc :: [a] -> [a] -> [a]. ele verifica que conc l m, que aparece no lado direito da equação, é do tipo [a],
+-- que é justamente o tipo exigido pelo ":" à direita do =. Assim, ele termina e chega a conclusão que a função é
+-- bem tipada. 
 
 -- Exercícios
 
