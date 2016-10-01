6967d6ae8e431f81ceaa63364850818f78631087
Paulo Borba <phmb@cin.ufpe.br>
2016-10-01 11:59:06 -0300

    Ajustes finais para nova hierarquia de monads

--- src/ExistentialTypes.hs
+++ src/ExistentialTypes.hs
@@ -1,7 +1,10 @@
 {-# LANGUAGE ExistentialQuantification #-}
 module ExistentialTypes where
 
--- Na linguagem anterior, só elementos de Double podiam ser usados como literais. 
+import Control.Applicative (Applicative(..))
+import Control.Monad       (liftM, ap)
+
+-- Na linguagem anterior, só elementos de Double podiam ser usados como literais.
 -- Uma opção seria criar um construtor para cada tipo de literal, o que geraria uma
 -- certa replicação do código de avaliação dos mesmos. Outra opção seria assumir que
 -- literais são strings e aí, na avaliação dos mesmos, fazer o parsing da string para
@@ -30,25 +33,31 @@ instance Interpretable Bool where
 -- Note que a semântica é bem diferente do que se "a" fosse um parâmetro do tipo
 -- Termo, como em data Termo a = ...
 -- Nesse caso, teríamos um gerador de linguagem. Para cada instanciação de Termo,
--- uma nova linguagem com seu tipo específico de literal. Mas nos programas de 
+-- uma nova linguagem com seu tipo específico de literal. Mas nos programas de
 -- uma dada linguagem não seria possível usar literais de tipos diferentes, como
 -- Double e Bool, por exemplo.
-    
+
 
 data Valor = Num Double
            | Boolean Bool
-           | Fun (Valor -> StateTransformer Valor) 
+           | Fun (Valor -> StateTransformer Valor)
            | Erro
 
 
 data StateTransformer a = ST (Estado -> (a,Estado))
 
+instance Functor (StateTransformer) where
+    fmap = liftM
+
+instance Applicative (StateTransformer) where
+    pure r = ST (\e -> (r,e))
+    (<*>) = ap
+
 instance Monad (StateTransformer) where
-   return r = ST (\e -> (r,e))
    (ST m) >>= f = ST (\e -> let (v,e1) = m e
                                 (ST n) = f v
                             in (n e1)
-                     ) 
+                     )
 
 type Estado = [(Id,Valor)]
 
@@ -77,11 +86,11 @@ int a (Apl f t) = do f1 <- int a f
 int a (Atr i t) = do v <- int a t
                      assign (i,v)
 
-		 {- = ST (\e -> let (ST f) = int a t 
+{- = ST (\e -> let (ST f) = int a t
                                 (v,ei) = f e
-                            in (v,wr (i,v) ei))  
+                            in (v,wr (i,v) ei))
                   -}
-		     
+
 int a (Seq t u) = do int a t
                      int a u
 
@@ -89,7 +98,7 @@ int a (Seq t u) = do int a t
 assign (i,v) = ST (\e -> (v,wr (i,v) e))
 
 search i [] = Erro
-search i ((j,v):l) = if i == j then v else search i l  
+search i ((j,v):l) = if i == j then v else search i l
 
 somaVal (Num x) (Num y) = Num (x + y)
 somaVal _ _ = Erro
@@ -125,12 +134,12 @@ instance Show Valor where
 
 
 
--- Ao invés de interpretar as expressões e obter o resultado final das suas execuções, 
+-- Ao invés de interpretar as expressões e obter o resultado final das suas execuções,
 -- podemos também só analisar o programa para verificar propriedades mais simples, como
 -- para onde flui informações sigilosas. Assim, não precisamos armazenar os valores das
 -- variáveis, mas apenas a informação de se ela contém informação sigilosa ou não.
--- Temos também considerar o caso de falha, e da variável armazenar uma função. É 
--- necessário armazenar a função para depois saber se informação é vazada na aplicação 
+-- Temos também considerar o caso de falha, e da variável armazenar uma função. É
+-- necessário armazenar a função para depois saber se informação é vazada na aplicação
 -- dessa função.
 
 data Result = B Bool | F ((Result,FlowState) -> (Result,FlowState)) | Falha
@@ -146,16 +155,22 @@ instance Show Result where
     show (B x) = show x
     show Falha = "Falha"
     show (F _) = "Função"
-    
+
 
 data TaintTransformer a = TT (FlowState -> (a,FlowState))
 
+instance Functor (TaintTransformer) where
+    fmap = liftM
+
+instance Applicative (TaintTransformer) where
+    pure r = TT (\e -> (r,e))
+    (<*>) = ap
+
 instance Monad (TaintTransformer) where
-   return r = TT (\e -> (r,e))
    (TT m) >>= f = TT (\e -> let (v,e1) = m e
                                 (TT n) = f v
                             in (n e1)
-                     )    
+                     )
 
 
 -- adicionar if e while na linguagem
@@ -171,20 +186,20 @@ ana (Som t u) = do t1 <- ana t
                    return (orr t1 u1)
 
 ana (Atr i t) = do v <- ana t
-                   add i v 
+                   add i v
                    return v
 
 ana (Lam i t) = return (let (TT f) = ana t
-                        in (F (\(v,tainted) -> removePair i (f (wr (i,v) tainted)))))                   
+                        in (F (\(v,tainted) -> removePair i (f (wr (i,v) tainted)))))
 
 ana (Apl f t) = do f1 <- ana f
                    t1 <- ana t
                    appp f1 t1
-		     
+
 ana (Seq t u) = do ana t
                    ana u
 
-appp (F f) r = TT (\tainted -> f (r,tainted)) 
+appp (F f) r = TT (\tainted -> f (r,tainted))
 appp _ _ = return Falha
 
 orr (B x) (B y) = (B (x || y))
@@ -194,10 +209,10 @@ add i v = TT (\t -> (v,wr (i,v) t))
 
 removePair i (r,l) = (r,remove i l)
 
-remove i l = [e | e <- l , i /= (fst e)]   
+remove i l = [e | e <- l , i /= (fst e)]
 
 get i [] = Falha
-get i ((j,v):l) = if i == j then v else get i l  
+get i ((j,v):l) = if i == j then v else get i l
 
 p1 = Atr "x" (Var "x")
 p2 = Atr "y" (Var "x")
@@ -214,5 +229,5 @@ p9 = Seq p1 (Seq p2 (Seq p3 (Seq p4 (Seq p5 (Seq p6 (Seq p7 p8))))))
 -- da execução, e se o resultado final gerado pelo programa vaza informação
 -- sigilosa
 
-analisador p = let (TT f) = ana p 
+analisador p = let (TT f) = ana p
                in f [("x",(B True))]
